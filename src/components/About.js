c994620c6a9e18cca05ac4d47d097133b648fa7f
xinghao23 <xinghao23@hotmail.com>
2024-10-31 00:49:06 -0700

    updated contents, added awardspage, added about

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,86 @@
 // src/components/About.js
 import React from 'react';
+import Xinghao from "../img/Xinghao.JPG";
+import Nevin from "../img/Nevin.png"
 
-function About() {
+const people = [
+    { id: 1, 
+      name: "Xinghao", 
+      photo: Xinghao, 
+      title: "Coach",
+      description: "Xinghao has been involved with VEX Robotics since 2014. Xinghao is a graduate of the Robosavages, the former world champion robotics team at Gladstone Secondary. Xinghao was the VEX Robotics BC Provincial Champion in 2016 and 2017. He was the frequent winner of the Design and Tournament Champion when he competed and has qualified to the World Championship eight times. Xinghao has been a coach in VEX Robotics for 7+ years and has mentored over 50+ students each year. He was the former lead coach and coordinator at Roboplanets before founding Western Pacific Robotics Academy. He has a Mechatronics and Business Double Major at SFU. Xinghao leads our courses and student coordinating, communications, and competitive teams programming. " 
+
+    },
+    { id: 2, 
+      name: "Nevin", 
+      photo: Nevin, 
+      title: "Coach",
+      description: "Nevin has been involved with VEX robotics since 2011. Nevin is a graduate of the Robosavages, the former world champion robotics team at Gladstone Secondary. Nevin was the VEX Robotics BC Provincial Champion in 2014 and 2015. He was the frequent winner of the Excellence, Design, and Tournament Champion when he competed, winning 32 awards. Nevin has been the Event Partner of local championships for 10+ years before founding Western Pacific Robotics Academy. Nevin sits on the Board of Directors of Pacific Youth Robotics Society. Nevin has a Bachelors of Commerce from UBC Sauder School of Business and successful CPA CFE exam writer. Nevin leads our competitive team mentorship, tournaments, and accounting." 
+
+    },
+    { id: 3, name: "Winson", 
+      photo: Xinghao, 
+      title: "Coach",
+      description: "Winson has been involved with VEX Robotics since 2015. Winson is a graduate of the Robosavages, the former world champion robotics team at Gladstone Secondary. Winson was the VEX Robotics BC Provincial Champion in 2017. He has qualified to the World Championships seven times. He is completing a Bachelor of Science in Applied Computer Science at BCIT. Winson has been a coach in VEX Robotics for 7+ years and has mentored over 60+ students each year. He was a former lead coach at Roboplanets before founding Western Pacific Robotics Academy. Winson leads our programming and competitive courses. ." 
+
+    },
+    { id: 4, 
+      name: "Charles", 
+      photo: Xinghao, 
+      title: "Coach",
+      description: "Charles bring a wealth of experience from participating in numerous professional robotics competitions as a former competitor at Seaquam Secondary robotics. He has hands-on expertise guiding students in achieving exceptional results in VEX IQ. He has successfully led teams to the Robotics World Championships, with his IQ team winning World Finalist in 2023. He is passionate about fostering a collaborative and innovative environment that inspires students to excel in robotics. Charles has a Bachelor’s Degree in Information Systems at Kwantlen Polytechnic University. Charles was a former coach at Roboplanets before joining Western Pacific Robotics Academy. Charles leads our introduction courses and junior teams." 
+
+    },
+    { id: 5, 
+      name: "Kenny", 
+      photo: Xinghao, 
+      title: "Coach",
+      description: "Kenny is a proud graduate of the Robosavages, the former world champion robotics team at Gladstone Secondary. He achieved success as the VEX Robotics BC Provincial Champion in 2018 and 2019, frequently earning Excellence and Tournament Champion awards during his competitive career, and qualifying for the World Championship. Currently, he is pursuing a Bachelor of Applied Science in Mechatronics Engineering at Simon Fraser University, where he combines his passion for robotics with advanced technical knowledge." 
+
+    },
+    { id: 6, 
+      name: "Sammy", 
+      photo: Xinghao, 
+      title: "Coach",
+      description: "Sammy is a graduate of the Robosavages, the former world champion robotics team at Gladstone Secondary. He achieved notable success as the VEX Robotics BC Provincial Champion in 2019 and 2020, frequently winning Excellence and Tournament Champion awards during his competitive career and qualifying for the World Championship. Currently, he is pursuing a Bachelor of Applied Science in Mechatronics Engineering at Simon Fraser University, where he is deepening his knowledge and passion for robotics." 
+
+    },
+    { id: 7, 
+      name: "Stanley", 
+      photo: Xinghao, 
+      title: "Coach",
+      description: "Stanley is a graduate of the Robosavages, the former world champion robotics team at Gladstone Secondary. Stanley has been teaching for 4+ years in robotics, math, chess, and english. He is pursuing his Bachelor of Applied Science in Mechatronics Engineering at SFU.  " 
+
+    },
+];
+
+const About = () => {
   return (
-    <div>
-      <h2>About Us</h2>
-      <p>This website was built using React!</p>
-    </div>
+      <div className="container">
+          {/* First Row with 3 People */}
+          <div className="row">
+              {people.slice(0, 3).map(person => (
+                  <div key={person.id} className="person-box">
+                      <img src={person.photo} alt={person.name} className="photo" />
+                      <div className="name">{person.name}</div>
+                      <div className="title">{person.title}</div> {/* Added title */}
+                      <div className="description">{person.description}</div>
+                  </div>
+              ))}
+          </div>
+          {/* Second Row with 4 People */}
+          <div className="row">
+              {people.slice(3, 7).map(person => (
+                  <div key={person.id} className="person-box">
+                      <img src={person.photo} alt={person.name} className="photo" />
+                      <div className="name">{person.name}</div>
+                      <div className="title">{person.title}</div> {/* Added title */}
+                      <div className="description">{person.description}</div>
+                  </div>
+              ))}
+          </div>
+      </div>
   );
-}
+};
 
 export default About;
--- src/components/AwardsPage.js
+++ src/components/AwardsPage.js
@@ -2,10 +2,78 @@
 import React from 'react';
 import backgroundVideo from '../video/awardsVideo.mp4';
 
-const awardsData = [
-  { title: 'Best Innovation Award', year: 2022, description: 'Awarded for outstanding innovation in technology.' },
-  { title: 'Community Impact Award', year: 2021, description: 'Recognized for positive contributions to the community.' },
-  { title: 'Excellence in Design', year: 2020, description: 'Honored for exceptional design standards in projects.' },
+const awardsDataHighStakes = [
+  { title: 'Minnesota Signature Event Build Award', team: "886W", description: 'Aaron, Ethan, Thomas'  },
+  { title: 'WPRA V5RC High Stakes Early Season Qualifier Excellence Award', team: "886W", description: 'Aaron, Ethan, Thomas'  },
+  { title: 'WPRA V5RC High Stakes Early Season Qualifier Tournament Champion', team: "886W", description: 'Aaron, Ethan, Thomas'  },
+  { title: 'WPRA V5RC High Stakes Early Season Qualifier Tournament Finalist', team: "77174B", description: 'Alan, Allan, Oscar, Wilson'  },
+  { title: 'WPRA V5RC High Stakes Early Season Qualifier Create Award', team: "886S", description: 'Aaron, Braden, Jimmy, Victoria'  },
+  { title: 'WPRA V5RC High Stakes Early Season Qualifier Judges Award', team: "886U", description: 'Andre, Justin, Kendrick'  },
+  { title: 'WPRA V5RC High Stakes Season Opener Tournament Champion', team: "886W", description: 'Aaron, Ethan, Thomas'  },
+  { title: 'WPRA V5RC High Stakes Season Opener Excellence Award', team: "886W", description: 'Aaron, Ethan, Thomas'  },
+  { title: 'WPRA V5RC High Stakes Season Opener Robot Skills Champion', team: "886W", description: 'Aaron, Ethan, Thomas'  },
+  { title: 'WPRA V5RC High Stakes Season Opener Tournament Finalist', team: "886R", description: 'Eddie, Laurence, Tina'  },
+  { title: 'WPRA V5RC High Stakes Season Opener Design Award', team: "886R", description: 'Eddie, Laurence, Tina'  },
+  { title: 'WPRA V5RC High Stakes Season Opener Innovative', team: "886Z", description: 'Evan, Julianne, Sharon'  },
+  { title: 'WPRA V5RC High Stakes Season Opener Create Award', team: "77174B", description: 'Alan, Allan, Oscar, Wilson'  },
+  { title: 'WPRA V5RC High Stakes Season Opener Judges Award', team: "886Y", description: 'Alan, Ariel, Mo'  },
+  { title: 'WPRA V5RC High Stakes Halloween Qualifier Tournament Champion', team: "886Y", description: 'Alan, Ariel, Mo'  },
+  { title: 'WPRA V5RC High Stakes Halloween Qualifier Tournament Finalist', team: "886S", description: 'Eddie, Laurence, Tina'  },
+  { title: 'WPRA V5RC High Stakes Halloween Qualifier Design Award', team: "886Y", description: 'Alan, Ariel, Mo'  },
+  { title: 'WPRA V5RC High Stakes Halloween Qualifier Innovate Award', team: "886R", description: 'Eddie, Laurence, Tina'  },
+  { title: 'WPRA V5RC High Stakes Halloween Qualifier Judges Award', team: "886S", description: 'Aaron, Braden, Jimmy, Victoria'  },
+
+  // Add more awards here as needed
+];
+const awardsDataOverUnder = [
+  { title: 'WPRA Early Season Summer Qualifier Design Award', team: "886N", description: 'Alan, Braden'},
+  { title: 'WPRA Early Season Summer Qualifier Create Award', team: "886A", description: 'Eddie, Simone, Oscar'},
+  { title: 'WPRA Season Opener Qualifier Build Award', team: "886Z", description: 'Evan, Hubert, Jun, William, Sharon'},
+  { title: 'PYRS BC Season Opener Tournament Champion', team: "886N", description: 'Alan, Braden'},
+  { title: 'PYRS BC Season Opener Amaze Award', team: "604X", description: 'Alan, Ethan, Victoria'},
+  { title: 'WPRA Halloween VRC Qualifier Tournament Finalist', team: "604X", description: 'Alan, Ethan, Victoria'},
+  { title: 'WPRA Halloween VRC Qualifier Design Award', team: "604X", description: 'Alan, Ethan, Victoria'},
+  { title: 'PYRS BC VRC Fall Qualifer Excellence Award', team: "604X", description: 'Alan, Ethan, Victoria'},
+  { title: 'PYRS BC VRC Fall Qualifer Tournament Finalist', team: "604X", description: 'Alan, Ethan, Victoria'},
+  { title: 'PYRS BC VRC Fall Qualifer Judges Award', team: "886W", description: 'Aaron, Chares, Eddie, Thomas'},
+  { title: 'RoboPlanet Long Weekend Special Tournament Champion', team: "886W", description: 'Aaron, Chares, Eddie, Thomas'},
+  { title: 'PYRS VRC Fall Qualifier Collingwood School Excellence Award', team: "886W", description: 'Aaron, Chares, Eddie, Thomas'},
+  { title: 'PYRS VRC Fall Qualifier Collingwood School Tournament Champion', team: "886N", description: 'Alan, Braden'},
+  { title: 'PYRS VRC Fall Qualifier Collingwood School Tournament Finalist', team: "604X", description: 'Alan, Ethan, Victoria'},
+  { title: 'PYRS VRC Fall Qualifier Collingwood School Create Award', team: "77174B", description: 'Alan, Allan, Oscar'},
+  { title: 'PYRS VRC Fall Qualifier Collingwood School Judges Award', team: "604Y", description: 'Alyssa, Laurence, Moses, Julianne'},
+  { title: 'PYRS VRC Heritage Woods Qualifer Excellence Award', team: "886Y", description: 'Barry, Cooper'},
+  { title: 'PYRS VRC Heritage Woods Qualifer Tournament Champion', team: "886N", description: 'Alan, Braden'},
+  { title: 'PYRS VRC Heritage Woods Qualifer Robot Skills Champion', team: "886Y", description: 'Barry, Cooper'},
+  { title: 'PYRS VRC Heritage Woods Qualifer Tournament Finalist', team: "886Y", description: 'Barry, Cooper'},
+  { title: 'PYRS VRC Heritage Woods Qualifer Amaze Award', team: "604X", description: 'Alan, Ethan, Victoria'},
+  { title: 'PYRS VRC Heritage Woods Qualifer Build Award', team: "886N", description: 'Alan, Braden'},
+  { title: 'Gord Trousdell Ten Ton Robotics VRC Middle School Tournament Champions', team: "886W", description: 'Aaron, Chares, Eddie, Thomas'},
+  { title: 'Gord Trousdell Ten Ton Robotics VRC Middle School Design Award', team: "886W", description: 'Aaron, Chares, Eddie, Thomas'},
+  { title: 'WPRA Christmas VRC Qualifier Excellence Award', team: "886W", description: 'Aaron, Chares, Eddie, Thomas'},
+  { title: 'WPRA Christmas VRC Qualifier Create Award', team: "77174B", description: 'Alan, Allan, Oscar'},
+  { title: 'WPRA New Year VRC Qualifier Excellence Award', team: "77174B", description: 'Alan, Allan, Oscar'},
+  { title: 'WPRA New Year VRC Qualifier Tournament Champion', team: "77174B", description: 'Alan, Allan, Oscar'},
+  { title: 'WPRA New Year VRC Qualifier Tournament Champion', team: "886N", description: 'Alan, Braden'},
+  { title: 'WPRA New Year VRC Qualifier Tournament Finalist', team: "886Z", description: 'Evan, Hubert, Jun, William, Sharon'},
+  { title: 'WPRA New Year VRC Qualifier Design Award', team: "886N", description: 'Alan, Braden'},
+  { title: 'WPRA New Year VRC Qualifier Build Award', team: "604Y", description: 'Alyssa, Laurence, Moses, Julianne'},
+  { title: 'Ignite the Northwest VRC Signature Event Sportsmanship Award', team: "886N", description: 'Alan, Braden'},
+  { title: 'PYRS BC VRC Winter Qualifier Tournament Champion', team: "886Y", description: 'Alan, Barry, Cooper'},
+  { title: 'PYRS BC VRC Winter Qualifier Tournament Finalist', team: "77174B", description: 'Alan, Allan, Oscar'},
+  { title: 'PYRS BC VRC Winter Qualifier Think Award', team: "77174B", description: 'Alan, Allan, Oscar'},
+  { title: 'PYRS BC VRC Winter Qualifier Judges Award', team: "886Y", description: 'Alan, Barry, Cooper'},
+  { title: 'Ten Ton Robotics Last Chance Tournament Champion', team: "886V", description: 'Aaron, Daniel, Messi, Claire'},
+  { title: 'WPRA Last Stan VRC Qualifier Excellence Award', team: "886W", description: 'Aaron, Chares, Eddie, Thomas'},
+  { title: 'WPRA Last Stan VRC Qualifier Tournament Champion', team: "886W", description: 'Aaron, Chares, Eddie, Thomas'},
+  { title: 'WPRA Last Stan VRC Qualifier Tournament Champion', team: "604X", description: 'Alan, Ethan, Victoria'},
+  { title: 'WPRA Last Stan VRC Qualifier Robot SKills Champion', team: "886W", description: 'Aaron, Chares, Eddie, Thomas'},
+  { title: 'WPRA Last Stan VRC Qualifier Build Award', team: "604X", description: 'Alan, Ethan, Victoria'},
+  { title: 'PYRS BC Mainland HS VRC Championship Amaze Award', team: "886Y", description: 'Alan, Barry, Cooper'},
+  { title: 'PYRS BC Mainland HS VRC Championship Build Award', team: "77174B", description: 'Alan, Allan, Oscar'},
+  { title: 'PYRS BC Mainland MS VRC Championship Tournament Champion', team: "886W", description: 'Aaron, Chares, Eddie, Thomas'},
+  { title: '2024 VEX Robotics World Championship Division Finalist', team: "886Y", description: 'Alan, Cooper, Ethan, Jimmy, Jun'},
+
   // Add more awards here as needed
 ];
 
@@ -16,17 +84,32 @@ const AwardsPage = () => {
         <source src={backgroundVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="overlay"></div> {/* Overlay layer */}
+      <div className="overlay"></div>
       <div className="awards-content">
         <h1 className="awards-title">Our Awards</h1>
-        <div className="awards-grid">
-          {awardsData.map((award, index) => (
-            <div key={index} className="award-card">
-              <h2>{award.title}</h2>
-              <p><strong>Year:</strong> {award.year}</p>
-              <p>{award.description}</p>
-            </div>
-          ))}
+        <div className="awards-column">
+          {/* Box 1 */}
+          <div className="award-box">
+            <h2>VEX VRC High Stakes Season</h2>
+            <ul>
+              {awardsDataHighStakes.map((award, idx) => (
+                <li key={idx}>
+                  <strong>{award.title} ({award.team}):</strong> {award.description}
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* Box 2 */}
+          <div className="award-box">
+            <h2>VEX VRC Over Under Season</h2>
+            <ul>
+              {awardsDataOverUnder.map((award, idx) => (
+                <li key={idx}>
+                  <strong>{award.title} ({award.team}):</strong> {award.description}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
